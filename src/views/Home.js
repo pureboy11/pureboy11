185e75be821f9830383c8bf07a6e0bf3fc9e1375
yongseok lee <greatyongseok@gmail.com>
2022-08-25 18:48:05 +0900

    Home 배경제거, 심플하게 구성

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,71 +1,24 @@
-import { Button, Form, InputGroup, ButtonGroup, Dropdown, DropdownButton, Container, Row, Col } from 'react-bootstrap';
-
-
+import React, { useState } from 'react';
+import WalletAddress from '../components/WalletAddress.js';
+import { Button } from 'react-bootstrap';
 
 function Home() {
+    const [address, setAddress] = useState(0);
+
+
     return (
         <div>
             <div className="home-img">
                 <h2 className="home-title">How much NFT assets do you have?</h2>
                 <h4 className="home-subtitle"> Check it right now by simply entering wallet address.</h4>
-                <div className="home-btn">
-                    <InputGroup>
-                        <ButtonGroup>
-                            <DropdownButton as={ButtonGroup} title="Wallet Address" id="bg-nested-dropdown">
-                                <Dropdown.Item eventKey="1">Metamask</Dropdown.Item>
-                                <Dropdown.Item eventKey="2">Kaikas</Dropdown.Item>
-                                <Dropdown.Item eventKey="3">Coinbase</Dropdown.Item>
-                                <Dropdown.Item eventKey="4">ZenGo</Dropdown.Item>
-                                <Dropdown.Item eventKey="5">Ronin</Dropdown.Item>
-                                <Dropdown.Item eventKey="6">etc</Dropdown.Item>
-                            </DropdownButton>
-
-                            <Form.Control
-                                placeholder="Enter any Wallet Address. We will Find your's."
-                                style={{ width: '500px' }}
-                                as="textarea"
-                                aria-label="With textarea"
-                            />
-                            <Button
-                                variant="info"
-                                size="lg"
-                            > check now </Button>
-                        </ButtonGroup>
-                    </InputGroup>
-                </div>
-            </div>
-            <div className="home-desc">
-                <h2> Our Goal</h2>
-                <Container>
-                    <Row>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                    </Row>
-                </Container>
-            </div>
-            <div className="home-desc">
-                <h2> Our History</h2>
-                <Container>
-                    <Row>
-                        <Col xs={8}><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                    </Row>
-                </Container>
-            </div>
-            <div className="home-desc">
-                <h2> Our Team</h2>
-                <Container>
-                    <Row>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                        <Col><div className="home-box" /></Col>
-                    </Row>
-                </Container>
+                <WalletAddress />
+                <Button
+                    variant="info"
+                    className="add-btn"
+                    style={{ borderRadius : '50px'}}
+                    onClick={ ()=>{ setAddress(+1)}}
+                    >+</Button>
+                    { address >= 1 ? <WalletAddress /> : null }
             </div>
         </div>
     )
